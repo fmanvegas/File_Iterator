80111ddadbf1050f5da9fdd6967bce1ccbc934c5
fmanvegas <fmanvegas@gmail.com>
2020-06-20 08:15:43 -0700

    Add project files.

---
+++ Holders.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace File_Iterator
+{
+    public class FileHolder : PropChanged
+    {
+        public FileInfo File { get; }
+
+        public ObservableCollection<LineHolder> Lines { get; private set; }
+
+        public FileHolder(FileInfo obj)
+        {
+            if (obj.FullName.Contains(".i.") ||
+                obj.FullName.Contains(".g."))
+                return;
+
+            File = obj;
+            Valid = true;
+            readLines();
+        }
+
+        private void readLines()
+        {
+            Lines = new ObservableCollection<LineHolder>();
+            var content = System.IO.File.ReadAllLines(File.FullName);
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(content[i]))
+                    continue;
+                Lines.Add(new LineHolder(this, i, content[i]));
+            }
+        }
+
+        public bool Valid = false;
+
+        private string _comment;
+
+        public string Comment
+        {
+            get { return _comment; }
+            set { _comment = value; OnPropChanged(); }
+        }
+
+
+        public bool IsDirty => Lines.FirstOrDefault(x => x.Bad) != null;
+    }
+
+    public class LineHolder : PropChanged
+    {
+        public LineHolder(FileHolder fileHolder, int i, string content)
+        {
+            Owner = fileHolder;
+            Index = i;
+            Content = content;
+
+            CommandSetBad = new RelayCommand(setBad);
+            CommandSetGood = new RelayCommand(setGood);
+        }
+        internal FileHolder Owner;
+        public int Index { get; }
+        public string Content { get; }
+
+        private bool _badLine;
+        public bool Bad
+        {
+            get { return _badLine; }
+            set { _badLine = value; OnPropChanged(); }
+        }
+        public RelayCommand CommandSetGood { get; }
+        private void setGood(object obj)
+        {
+            Owner.Lines.Remove(this);
+        }
+
+
+        public RelayCommand CommandSetBad { get; }
+
+        private void setBad(object obj)
+        {
+            Bad = !Bad;
+        }
+
+        public override string ToString()
+        {
+            return Content;
+        }
+    }
+}
---
+++ IteratorViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace File_Iterator
+{
+    public class IteratorViewModel : PropChanged
+    {
+        public IteratorViewModel() {
+
+            FinalizeFileCommand = new RelayCommand(finalizeFile);
+            SearchLocationCommand = new RelayCommand(searchLocation);
+        }
+
+       
+        private void searchLocation(object obj)
+        {
+            Directory = new DirectoryInfo(Location);
+
+            if (!Directory.Exists)
+                return;
+
+            var subDirectories = Directory.GetDirectories()
+                               .Where(d => (d.Attributes & FileAttributes.ReparsePoint) == 0
+                                      && (d.Attributes & FileAttributes.System) == 0);
+            var options = new EnumerationOptions()
+            {
+                RecurseSubdirectories = true,
+                IgnoreInaccessible = true
+            };
+
+
+            List<FileInfo> files = new List<FileInfo>();
+
+            foreach (var d in subDirectories)
+                files.AddRange(d.GetFiles("*.*", options).Where(s => s.Extension.EndsWith(".cs") || s.Extension.EndsWith(".xaml")));
+
+            files.ForEach(generateEntry);
+
+            TotalExtensions = (from f in files select f.Extension).Distinct().Count();
+
+            TotalFiles = Files.Count;
+        }
+
+      
+
+        private void finalizeFile(object obj)
+        {
+            if (!(obj is FileHolder holder))
+                return;
+
+            Files.Remove(holder);
+
+            if (holder.IsDirty)
+                BadFiles.Add(holder);
+            else
+                GoodFiles.Add(holder);
+        }
+        
+        public string Location
+        {
+            get => _location;
+            set { _location = value; OnPropChanged(); }
+        }
+        private string _location = @"C:\Users\fmanv\source\repos\BinarDataGenerator";
+
+
+        private DirectoryInfo Directory { get; set; }
+
+        private int _totalFiles;
+        public int TotalFiles
+        {
+            get { return _totalFiles; }
+            set { _totalFiles = value; OnPropChanged(); }
+        }
+
+        private int _totalExtensions;
+        public int TotalExtensions
+        {
+            get { return _totalExtensions; }
+            set { _totalExtensions = value; OnPropChanged(); }
+        }
+
+        private int _dirtyPercent = 30;
+        public int DirtyPercent
+        {
+            get { return _dirtyPercent; }
+            set { _dirtyPercent = value; OnPropChanged(); }
+        }
+
+        private int _dirtyCount = 789;
+        public int DirtyCount
+        {
+            get { return _dirtyCount; }
+            set { _dirtyCount = value; OnPropChanged(); }
+        }
+
+        public ObservableCollection<FileHolder> Files { get; set; } = new ObservableCollection<FileHolder>();
+        public ObservableCollection<FileHolder> GoodFiles { get; set; } = new ObservableCollection<FileHolder>();
+        public ObservableCollection<FileHolder> BadFiles { get; set; } = new ObservableCollection<FileHolder>();
+
+        public RelayCommand FinalizeFileCommand { get; }
+        public RelayCommand SearchLocationCommand { get; }
+
+        public FileHolder SelectedFile { get => _selectedFile; set { _selectedFile = value; OnPropChanged();  } }
+        private FileHolder _selectedFile;
+
+      
+        private void generateEntry(FileInfo obj)
+        {
+            var temp = new FileHolder(obj);
+            if (temp.Valid)
+                Files.Add(temp);
+        }
+    }
+
+   
+
+    public class PropChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
